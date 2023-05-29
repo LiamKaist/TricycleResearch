220718b952ec551112faabc42638f734d67103bb
LiamKaist <liamchrisment@gmail.com>
2023-05-29 19:38:38 +0200

    FIxed nested git problem

---
+++ src/LiDar/ros2_lidar_ws/build/rslidar_msg/rslidar_msg__py/CMakeFiles/rslidar_msg__py.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "/home/lilultime/ros2_lidar_ws/build/rslidar_msg/rosidl_generator_py/rslidar_msg/_rslidar_msg_s.ep.rosidl_typesupport_c.c"
+  "/home/lilultime/ros2_lidar_ws/build/rslidar_msg/rosidl_generator_py/rslidar_msg/_rslidar_msg_s.ep.rosidl_typesupport_fastrtps_c.c"
+  "/home/lilultime/ros2_lidar_ws/build/rslidar_msg/rosidl_generator_py/rslidar_msg/_rslidar_msg_s.ep.rosidl_typesupport_introspection_c.c"
+  "/home/lilultime/ros2_lidar_ws/build/rslidar_msg/rosidl_generator_py/rslidar_msg/msg/__init__.py"
+  "/home/lilultime/ros2_lidar_ws/build/rslidar_msg/rosidl_generator_py/rslidar_msg/msg/_rslidar_packet.py"
+  "/home/lilultime/ros2_lidar_ws/build/rslidar_msg/rosidl_generator_py/rslidar_msg/msg/_rslidar_packet_s.c"
+  "CMakeFiles/rslidar_msg__py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/rslidar_msg__py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
